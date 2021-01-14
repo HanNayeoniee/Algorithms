acd1c98b7a2f0f43766ef2f45a6f2d693a7ff9de
HanNayeoniee <piggutail79@gmail.com>
2021-01-14 11:21:46 +0900

    quick sort

--- level12_sorting/quick_sort.py
+++ level12_sorting/quick_sort.py
@@ -22,7 +22,31 @@ def quick_sort(data, start, end):
     quick_sort(data, start, j-1)
     quick_sort(data, j+1, end)
 
-data = [1, 10, 5, 8, 7, 6, 4, 3, 2, 9]
-quick_sort(data, 0, len(data)-1)
-print(data)
+# data = [1, 10, 5, 8, 7, 6, 4, 3, 2, 9]
+# quick_sort(data, 0, len(data)-1)
+# print(data)
 
+
+def quick_sort2(arr):
+    # 원소가 1개인 경우
+    if (len(arr) <= 1):
+        return arr
+
+    # 피벗은 리스트의 가운데 숫자
+    pivot = arr[len(arr) // 2]
+    small_arr, equal_arr, big_arr = [], [], []
+
+    for num in arr:
+        if num < pivot:
+            small_arr.append(num)
+        elif num > pivot:
+            big_arr.append(num)
+        else:
+            equal_arr.append(num)
+
+    # 작은 리스트, 큰 리스트 각각 재귀해 퀵정렬
+    return quick_sort2(small_arr) + equal_arr + quick_sort2(big_arr)
+
+arr=[3, 7, 8, 1, 5, 9, 6, 10, 2, 4]
+result=quick_sort2(arr)
+print(result)
