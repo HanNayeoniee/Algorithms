500c282c209821670062af54a47df084eece4b37
Na Yeon Han <piggutail79@gmail.com>
2021-02-15 11:05:34 +0900

    Create 2163.py

---
+++ acmicpc/2163.py
@@ -0,0 +1,15 @@
+  
+"""
+백준 2163. 초콜릿 자르기
+problem : https://www.acmicpc.net/problem/2163
+"""
+
+import sys
+input = sys.stdin.readline
+
+def solve(n, m):
+    return (n-1) + n * (m-1)
+
+if __name__ == "__main__":
+    n, m = map(int, input().strip().split())
+    print(solve(n, m))
