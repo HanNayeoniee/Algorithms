9aa652b9c37e0d70facd62b274f2a3b108783ca2
Na Yeon Han <piggutail79@gmail.com>
2021-02-18 10:15:38 +0900

    Create 2935.py

---
+++ acmicpc/2935.py
@@ -0,0 +1,13 @@
+"""
+백준 2935. 소음
+problem : https://www.acmicpc.net/problem/2935
+"""
+
+a = int(input())
+operation = input()
+b = int(input())
+
+if (operation == '+'):
+    print(a+b)
+if (operation == '*'):
+    print(a*b)
